de451416c60607f50f12c9198d2ca32464bd3152
Marko <markogidej@yahoo.com>
2024-05-21 12:30:57 -0400

    Finally got db Table to make association table

--- app.py
+++ app.py
@@ -10,3 +10,95 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+mysqlconnector://root:{db_passwo
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
+class Customer(db.Model):
+    __tablename__ = 'Customers'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(255), nullable = False)
+    email = db.Column(db.String(320))
+    phone = db.Column(db.String(15))
+    orders = db.relationship('Order', backref = 'customer')
+
+class CustomerSchema(ma.Schema):
+    name = fields.String(required=True)
+    email = fields.String(required=True)
+    phone = fields.String(required=True)
+
+    class Meta:
+        fields = ("name", "email", "phone", "id")
+
+customer_schema = CustomerSchema()
+customers_schema = CustomerSchema(many=True)
+
+class Order(db.Model):
+    __tablename__ = 'Orders'
+    id = db.Column(db.Integer, primary_key = True)
+    date = db.Column(db.Date, nullable = False)
+    customer_id = db.Column(db.Integer, db.ForeignKey('Customers.id'))
+
+class CustomerAccount(db.Model):
+    __tablename__ = 'Customer_Accounts'
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(255), unique = True, nullable = False)
+    password = db.Column(db.String(255), nullable = False)
+    customer_id = db.Column(db.Integer, db.ForeignKey('Customers.id'))
+    customer = db.relationship('Customer', backref = 'customer_account', uselist = False)
+
+order_product = db.Table('Order_Product',
+    db.Column('order_id', db.Integer, db.ForeignKey('Orders.id'), primary_key = True),
+    db.Column('product_id', db.Integer, db.ForeignKey('Products.id'), primary_key = True)
+)
+
+class Product(db.Model):
+    __tablename__ = 'Products'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(255), nullable = False)
+    price = db.Column(db.Float, nullable = False)
+    orders = db.relationship('Order', secondary = order_product, backref = db.backref('products'))
+
+
+@app.route('/customers', methods = ['GET'])
+def get_customers():
+    customers = Customer.query.all()
+    return customers_schema.jsonify(customers)
+
+@app.route("/customers", methods = ["POST"])
+def add_customer():
+    try:
+        customer_data = customer_schema.load(request.json)
+    except ValidationError as e:
+        print(f"Error: {e}")
+        return jsonify(e.messages), 400
+    
+    new_customer = (customer_data['name'], customer_data['email'], customer_data['phone'])
+    db.session.add(new_customer)
+    db.session.commit()
+    return jsonify({"message": "New customer added sucesfully"}), 201
+
+@app.route("/customers/<int:id>", methods = ["PUT"])
+def update_cusomter(id):
+    customer = Customer.query.get_or_404(id)
+    try:
+        customer_data = customer_schema.load(request.json)
+    except ValidationError as e:
+        return jsonify(e.messages), 400
+    
+    customer.name = customer_data['name']
+    customer.email = customer_data['email']
+    customer.phone = customer_data['phone']
+    db.session.commit()
+
+    return jsonify({"message": "Customer updated sucesfully"}), 200
+
+@app.route("/customers/<int:id>", methods = ["DELETE"])
+def delete_customer(id):
+    customer = Customer.query.get_or_404(id)
+    db.session.delete(customer)
+    db.session.commit()
+
+    return jsonify({"message": "Customer removed sucesfully"}), 200
+
+with app.app_context():
+    db.create_all()
+
+if __name__ == '__main__':
+    app.run(debug = True)
